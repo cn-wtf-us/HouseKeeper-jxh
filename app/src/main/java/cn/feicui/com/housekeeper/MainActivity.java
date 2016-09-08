55a719a65c4369c80ce93f067a39003469c21bf9
Administrator <1021306006@qq.com>
2016-09-08 13:30:58 +0800

    属性动画

--- app/src/main/java/cn/feicui/com/housekeeper/MainActivity.java
+++ app/src/main/java/cn/feicui/com/housekeeper/MainActivity.java
@@ -1,7 +1,11 @@
 package cn.feicui.com.housekeeper;
 
+import android.animation.AnimatorInflater;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ImageView sk= (ImageView) findViewById(R.id.iv_sekai);
+        AnimatorSet animator= (AnimatorSet) AnimatorInflater.loadAnimator(this,R.animator.animator_alpha);
+        animator.setTarget(sk);
+        animator.start();
     }
 }
